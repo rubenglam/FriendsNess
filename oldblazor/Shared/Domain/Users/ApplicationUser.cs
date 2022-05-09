4a77b707bdc587b3cdcb5844b0ca7d5f1f6657ac
RubenGlam <rubenbermudezsaez@gmail.com>
2022-05-10 00:16:15 +0200

    Add gitignore

--- oldblazor/Shared/Domain/Users/ApplicationUser.cs
+++
@@ -1,9 +0,0 @@
-﻿using FriendsNess.Shared.Domain.Exercices;
-using Microsoft.AspNetCore.Identity;
-
-namespace FriendsNess.Shared.Domain.Users;
-
-public class ApplicationUser : IdentityUser<int>
-{
-    public ICollection<UserExercice> UserExercices { get; set; }
-}
